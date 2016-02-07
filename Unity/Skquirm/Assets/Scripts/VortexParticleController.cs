7e44cb5dbc07ef8253b13b2df94b5e72bc66c04e
Yorie Nakayama <yorie_nakayama@hotmail.com>
2016-02-07 11:06:28 +0900

    add gitignore, add spiral particle effect, made the soap bottle pink so it is visible above the particle effect

---
+++ Unity/Skquirm/Assets/Scripts/VortexParticleController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class VortexParticleController : MonoBehaviour {
+  public float rotationSpeed;
+  public float pullStrength;
+  private ParticleSystem m_System;
+  private ParticleSystem.Particle[] m_Particles;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+  private void LateUpdate()
+  {
+    InitializeIfNeeded();
+
+    // GetParticles is allocation free because we reuse the m_Particles buffer between updates
+    int numParticlesAlive = m_System.GetParticles(m_Particles);
+
+    // Change only the particles that are alive
+    for (int i = 0; i < numParticlesAlive; i++)
+    {
+      Vector3 drift = new Vector3(m_Particles[i].position.z, 0, -1 * m_Particles[i].position.x);
+      Vector3 position = new Vector3(m_Particles[i].position.x, 0, m_Particles[i].position.z);
+      drift.Normalize();
+      position.Normalize();
+      m_Particles[i].velocity = rotationSpeed * drift + -pullStrength * position;
+    }
+
+    // Apply the particle changes to the particle system
+    m_System.SetParticles(m_Particles, numParticlesAlive);
+  }
+
+  void InitializeIfNeeded()
+  {
+    if (m_System == null)
+      m_System = GetComponent<ParticleSystem>();
+
+    if (m_Particles == null || m_Particles.Length < m_System.maxParticles)
+      m_Particles = new ParticleSystem.Particle[m_System.maxParticles];
+  }
+}
---
+++ Unity/Skquirm/Assets/Scripts/VortexParticleEffect.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VortexParticleEffect : MonoBehaviour {
+
+  public GameObject arm;
+  public int numArms;
+  public float radius;
+  // public float pullSpeed;
+  // public float driftSpeed;
+
+  private GameObject[] armObjects;
+
+	// Use this for initialization
+	void Start () {
+    armObjects = new GameObject[numArms];
+
+    // instantiate arms from prefab
+    for (int i = 0; i < numArms; i++){
+      Vector3 position = new Vector3(
+        radius * Mathf.Sin(i * 2 * Mathf.PI / numArms),
+        0,
+        radius * Mathf.Cos(i * 2 * Mathf.PI / numArms)
+        );
+      armObjects[i] = (GameObject) Instantiate(arm, transform.position + position, Quaternion.identity);
+      armObjects[i].transform.parent = transform;
+    }
+	}
+}
