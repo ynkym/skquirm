205c02d56aaea8bf4953bf098322467a211fa12d
Alan Ricardo <alanrgo@gmail.com>
2016-02-13 01:45:20 -0400

    project with offense item developed. Press X to activate

---
+++ Unity/Skquirm/Assets/GameController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject[] players_avatar;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
