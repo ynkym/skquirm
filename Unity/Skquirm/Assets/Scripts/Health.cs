4bf71201d6de1f47f6255e243911447a9bb9fff1
jomart001ss <jomart001ss@gmail.com>
2016-02-21 18:18:49 -0500

    life
    lifeupdate

--- Unity/Skquirm/Assets/Scripts/Health.cs
+++ Unity/Skquirm/Assets/Scripts/Health.cs
@@ -20,14 +20,6 @@ public class Health : MonoBehaviour
 	{
 
 		if (life == 2){
-			if(transform.position.y >= water.transform.position.y){
-				//rigid.body.AddForce(transform.down * force * 10); 
-				//transform.localPosition = new Vector3(0, 0, 0);
-				//rb.AddForce(0, -15, 0);
-				//rb.AddForce(transform.down * 10);
-				rb.AddForce(new Vector3(0, transform.position.y , 0) * -10);
-			}
-
 			//Debug.Log("lol");
 			gameObject.GetComponent<Renderer>().material.color = Color.red;
 			//barSoap.renderer.GetComponent<Renderer>().material.color = Color.black;
