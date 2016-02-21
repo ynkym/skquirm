339023e28ba3df25881a92038ed191fd4ed3cc7c
jomart001ss <jomart001ss@gmail.com>
2016-02-21 14:39:55 -0500

    Health and Damage Indication
    Health and Damage Indication

---
+++ Unity/Skquirm/Assets/Scripts/Health.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Health : MonoBehaviour
+{
+	public static int life;        // The player's life.
+	//public Material damaged;
+	void Awake ()
+	{
+		life = 3;
+	}
+
+
+	void Update ()
+	{
+
+		if (life == 2){
+			Debug.Log("lol");
+			gameObject.GetComponent<Renderer>().material.color = Color.red;
+			//barSoap.renderer.GetComponent<Renderer>().material.color = Color.black;
+		}
+
+		if (life <= 0){
+
+			//Application.LoadLevel("GameOver");
+		}
+
+	}
+}  
