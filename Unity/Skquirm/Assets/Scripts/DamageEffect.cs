14e2995bcf62fa64f57f9bf3aa41cc44d8f7d98e
Yorie Nakayama <yorie_nakayama@hotmail.com>
2016-02-22 05:34:28 +0900

    Use local variable for life management

--- Unity/Skquirm/Assets/Scripts/DamageEffect.cs
+++ Unity/Skquirm/Assets/Scripts/DamageEffect.cs
@@ -2,19 +2,18 @@
 using System.Collections;
 
 public class DamageEffect : MonoBehaviour {
-	private static int hitpoints;  
 	void Start(){
 
 	}
 
 	void Update () {
-		hitpoints = Health.life; 
 	}
 
 	void OnTriggerEnter(Collider other) {
 		if (other.gameObject.CompareTag("Player")){
 
-			Health.life -=1; 
+			other.gameObject.GetComponent<Health>().life -= 1;
+			//Health.life -=1;
 		}
 	}
 
