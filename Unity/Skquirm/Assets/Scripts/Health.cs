99aafbb33ce9ebd0f253a689a8aa99398f4dd724
alexcfyung <alexcfyung@hotmail.com>
2016-02-21 18:26:08 -0500

    a

--- Unity/Skquirm/Assets/Scripts/Health.cs
+++ Unity/Skquirm/Assets/Scripts/Health.cs
@@ -5,25 +5,37 @@ using System.Collections;
 public class Health : MonoBehaviour
 {
 	public int life;        // The player's life.
+
+    private int currentLife;
+    
+
 	//public Material damaged;
 	void Awake ()
 	{
 		life = 3;
 	}
 
+    void Start ()
+    {
+        currentLife = life;
+    }
+
+    void getDamaged ()
+    {
+        currentLife -= 1;
+        if (currentLife <= 0)
+        {
+            //Application.LoadLevel("GameOver");
+        }
+    }
 
 	void Update ()
 	{
 
-		if (life == 2){
+		if (currentLife == 2){
 			//Debug.Log("lol");
 			gameObject.GetComponent<Renderer>().material.color = Color.red;
 			//barSoap.renderer.GetComponent<Renderer>().material.color = Color.black;
-		}
-
-		if (life <= 0){
-
-			//Application.LoadLevel("GameOver");
 		}
 
 	}
