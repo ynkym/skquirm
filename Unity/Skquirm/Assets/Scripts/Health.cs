510d2955868d2863b3c92936db20bc83812d1bd9
jomart001ss <jomart001ss@gmail.com>
2016-02-21 17:20:11 -0500

    More life stuff
    More life stuff

--- Unity/Skquirm/Assets/Scripts/Health.cs
+++ Unity/Skquirm/Assets/Scripts/Health.cs
@@ -6,8 +6,12 @@ public class Health : MonoBehaviour
 {
 	public int life;        // The player's life.
 	//public Material damaged;
+	public int force; 
+	public GameObject water;
+	public Rigidbody rb;
 	void Awake ()
 	{
+		 rb = GetComponent<Rigidbody>();
 		life = 3;
 	}
 
@@ -16,13 +20,32 @@ public class Health : MonoBehaviour
 	{
 
 		if (life == 2){
+			if(transform.position.y >= water.transform.position.y){
+				//rigid.body.AddForce(transform.down * force * 10); 
+				//transform.localPosition = new Vector3(0, 0, 0);
+				//rb.AddForce(0, -15, 0);
+				//rb.AddForce(transform.down * 10);
+				rb.AddForce(new Vector3(0, transform.position.y , 0) * -10);
+			}
+
 			//Debug.Log("lol");
 			gameObject.GetComponent<Renderer>().material.color = Color.red;
 			//barSoap.renderer.GetComponent<Renderer>().material.color = Color.black;
 		}
 
-		if (life <= 0){
+		if (life == 1){
+			if(transform.position.y >= water.transform.position.y){
+				//rigid.body.AddForce(transform.down * force * 10); 
+				//transform.localPosition = new Vector3(0, 0, 0);
+				//rb.AddForce(0, -15, 0);
+				//rb.AddForce(transform.down * 10);
+				rb.AddForce(new Vector3(0, transform.position.y , 0) * -10);
+			}
 
+		}
+
+		if (life <= 0){
+			Destroy(this.gameObject);
 			//Application.LoadLevel("GameOver");
 		}
 
