1d89b30e1bc60f4cca311610e6ed4e2d20439188
alanrgo <alanrgo@gmail.com>
2016-02-21 18:19:37 -0500

    Return the nearest solved. Testing environment solved

--- Unity/Skquirm/Assets/Scripts/PlayerController.cs
+++ Unity/Skquirm/Assets/Scripts/PlayerController.cs
@@ -78,12 +78,15 @@ public class PlayerController : MonoBehaviour {
     // Fixed time step update, usually for physics, everything moved to updateMovement
     void FixedUpdate () {
         InputDevice inputDevice = (InputManager.Devices.Count > playerNum) ? InputManager.Devices[playerNum] : null;
-        if (inputDevice != null)
-        {
-            if (!testingObj) {
-                UpdateMovement(inputDevice);
-            }
-        }
+		if (inputDevice != null) {
+			if (!testingObj) {
+				UpdateMovement (inputDevice);
+			}
+		} else {
+			if (!testingObj && TestWithoutJoystick) {
+				UpdateMovement (null);
+			}
+		}
 	}
 
 	void PickupItem (string newType) {
