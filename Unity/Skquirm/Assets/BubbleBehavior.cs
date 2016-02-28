f4700f6979822de40fafebdfb639817bdff04444
Alan Ricardo Gomes <Alan Ricardo Gomes>
2016-02-28 10:48:56 -0400

    bubble scaled

--- Unity/Skquirm/Assets/BubbleBehavior.cs
+++ Unity/Skquirm/Assets/BubbleBehavior.cs
@@ -7,7 +7,11 @@ public class BubbleBehavior : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         Vector3 start_scale = Vector3.zero;
+        Vector3 position = transform.localPosition;
 
+        position.y = 0.86f;
+
+        transform.localPosition = position;
         iTween.ScaleFrom(gameObject, iTween.Hash("scale", start_scale, "easetype", "spring", "time", 0.5f));	
 	}
 	
