3ced007036d9953f94c477af252c0011b4e1d5c7
RyanFoxMDFung <ryanshiuhong.fung@mail.utoronto.ca>
2016-03-05 17:30:45 -0500

    committing scripts for obstacle stuff
    -spawns obstacles randomly at y co-ord: 100
    -deals damage when it lands on a player
    -buggy with collision afterwards, it wont turn ragdolling off. Could
    someone take a look into this? Its in the ObstacleBehvaiour.cs

---
+++ Unity/Skquirm/Assets/Scripts/ObstacleBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleBehaviour : MonoBehaviour
+{
+    private ObstacleSpawn obstaclespawn;
+    public Rigidbody rb; //rigidbody is being strange?
+    // Use this for initialization
+    void Start()
+    {
+        obstaclespawn = GetComponent<ObstacleSpawn>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.transform.position.y < 3.6F)
+        {
+            //this is to disable rag dolling after it hits the water/reaches below a certain point in Y-coordinate
+            rb.isKinematic = true;
+            rb.detectCollisions = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //collision case of an obstacle with the event horizon.
+        if (collision.gameObject.tag == "EHorizon")
+        {
+            obstaclespawn.currentNumOfObs -= 1;
+        }
+        //collision case of a meteor obstacle vs. player
+        if (collision.gameObject.tag == "Player")
+        {
+            if (gameObject.transform.position.y > 3.6F)
+            {
+                collision.gameObject.GetComponent<PlayerController>().TryToHurt();
+            }
+        }
+    }
+}
