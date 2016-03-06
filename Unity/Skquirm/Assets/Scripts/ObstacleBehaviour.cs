59d94ddc558b1025d78219088b3085eed38ac711
RyanFoxMDFung <ryanshiuhong.fung@mail.utoronto.ca>
2016-03-06 15:36:35 -0500

    obstacles swirl after they hit the water

--- Unity/Skquirm/Assets/Scripts/ObstacleBehaviour.cs
+++ Unity/Skquirm/Assets/Scripts/ObstacleBehaviour.cs
@@ -5,9 +5,11 @@ public class ObstacleBehaviour : MonoBehaviour
 {
     private ObstacleSpawn obstaclespawn;
     public Rigidbody rb; //rigidbody is being strange?
+    private VortexForce vf;
     // Use this for initialization
     void Start()
     {
+        vf = GetComponent<VortexForce>();
         obstaclespawn = GetComponent<ObstacleSpawn>();
         rb = GetComponent<Rigidbody>();
     }
@@ -15,16 +17,18 @@ public class ObstacleBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*
+        
             //this portion of the code comes into play after the meteor object lands onto the water, at which then it is harmless, simply acts as a wall for players
             //it works, but it doesnt look good atm. Uncomment the code to see the results 
             if (gameObject.transform.position.y < 3.6F)
             {
                 //this is to disable rag dolling after it hits the water/reaches below a certain point in Y-coordinate
-                rb.isKinematic = true;
+                //rb.isKinematic = true;
+                vf.whirlStrength = 0.5f;
+                vf.pullStrength = 0.001f;
                 //rb.detectCollisions = false;
             }
-        */
+        
     }
 
     void OnCollisionEnter(Collision collision)
@@ -33,6 +37,7 @@ public class ObstacleBehaviour : MonoBehaviour
         if (collision.gameObject.tag == "EHorizon")
         {
             obstaclespawn.currentNumOfObs -= 1;
+            print(obstaclespawn.currentNumOfObs);
         }
         //collision case of a meteor obstacle vs. player
         if (collision.gameObject.tag == "Player")
