3d761cca618fdaba9d6d5200e227ba1e595197aa
Yorie Nakayama <yorie_nakayama@hotmail.com>
2016-03-06 13:02:03 +0900

    Revert to Alpha

--- Unity/Skquirm/Assets/Scripts/Health.cs
+++ Unity/Skquirm/Assets/Scripts/Health.cs
@@ -6,21 +6,16 @@ public class Health : MonoBehaviour
 {
 	public int life;        // The player's life.
 
-    private bool invincible = false;
+    //private bool invincible = false;
 
     private int currentLife;
 
-    public int invincibilityFrames;
 	public LifeStateUI lifeUI;
     public Renderer bottleRenderer;
 
 	//public Material damaged;
-	public int force;
-	public GameObject water;
-	public Rigidbody rb;
 	void Awake ()
 	{
-		 rb = GetComponent<Rigidbody>();
 		life = 3;
 	}
 
@@ -29,54 +24,35 @@ public class Health : MonoBehaviour
         currentLife = life;
     }
 
-    public IEnumerator getDamaged()
+    public void getDamaged ()
     {
-        if (!invincible)
-        {
+       // if (!invincible) **change void to IEnumerator if we're using this.
+        //{
             currentLife -= 1;
-            lifeUI.UpdateUI(currentLife);
-            if (currentLife <= 0)
+        lifeUI.UpdateUI(currentLife);
+        if (currentLife <= 0)
+        {
+            //Application.LoadLevel("GameOver");
+            GlobalSetting.Instance.SendMessage("PlayerDefeated", this.gameObject);
+            gameObject.SetActive(false);
+            Debug.Log("GameOver");
+        }
+        else {        
+            if (currentLife == 2 && bottleRenderer != null)
             {
-                //Application.LoadLevel("GameOver");
-                GlobalSetting.Instance.SendMessage("PlayerDefeated", this.gameObject);
-                gameObject.SetActive(false);
-                Debug.Log("GameOver");
+                //Debug.Log("lol");
+                bottleRenderer.material.color = Color.red;
             }
-            else {
-                if (currentLife == 2 && bottleRenderer != null)
-                {
-                    //Debug.Log("lol");
-                    bottleRenderer.material.color = Color.red;
-                }
-                else if (currentLife == 1 && bottleRenderer != null)
-                {
-                    bottleRenderer.material.color = Color.black;
-                }
-
-            		if (life == 1){
-            			if(transform.position.y >= water.transform.position.y){
-            				//rigid.body.AddForce(transform.down * force * 10);
-            				//transform.localPosition = new Vector3(0, 0, 0);
-            				//rb.AddForce(0, -15, 0);
-            				//rb.AddForce(transform.down * 10);
-            				rb.AddForce(new Vector3(0, transform.position.y , 0) * -10);
-            			}
-
-            		}
-
-            		if (life <= 0){
-            			Destroy(this.gameObject);
-            			//Application.LoadLevel("GameOver");
-            		}
+            else if (currentLife == 1 && bottleRenderer != null)
+            {
+                bottleRenderer.material.color = Color.black;
             }
 
-      }
-        invincible = true;
-        //print("INVINCIBLE");
-        yield return new WaitForSeconds(invincibilityFrames); //waits for 3 seconds before changing invincibility back to false
-        invincible = false;
-        //print("Not Invincible...");
+        }
 
+        //invincible = true;
+        //yield return new WaitForSeconds(3); //waits for 3 seconds before changing invincibility back to false
+        // invincible = false;
+        //}
     }
-
 }
--- Unity/Skquirm/Assets/Scripts/PlayerController.cs
+++ Unity/Skquirm/Assets/Scripts/PlayerController.cs
@@ -119,7 +119,6 @@ public class PlayerController : MonoBehaviour {
         if (barrier == null) {
             // Do actual damage
             health.getDamaged();
-            StartCoroutine(health.getDamaged());
             // quick hack: shift the position vertically to make bouncing due to buoyancy happen
             rb.transform.position = rb.transform.position + new Vector3(0, -1, 0);
             print("caused damage to the player");
