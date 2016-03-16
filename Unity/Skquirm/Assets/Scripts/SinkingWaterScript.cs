b88ab1453764cff678bdc2092e8dccbe944151e0
Alan Ricardo Gomes <Alan Ricardo Gomes>
2016-03-16 16:23:26 -0500

    tank changes

---
+++ Unity/Skquirm/Assets/Scripts/SinkingWaterScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SinkingWaterScript : MonoBehaviour {
+
+    public GameObject fstLayer, sndLayer;
+
+    public float fstReleaseYValue;
+    public float sndReleaseYValue;
+    public float sinkSpeed;
+
+    bool triggered;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void FixedUpdate() {
+        if (triggered)
+        {
+            transform.Translate(Vector3.up * sinkSpeed);
+            if( transform.position.y >= sndReleaseYValue && sndLayer.transform.parent == transform )
+            {
+                sndLayer.transform.parent = transform.parent;
+            }
+            else if(transform.position.y >= fstReleaseYValue && fstLayer.transform.parent == transform )
+            {
+                fstLayer.transform.parent = transform.parent;
+            }
+        }
+    }
+
+    [ContextMenu("Trigger Sinking Environment")]
+    public void TriggerSinkingWater() {
+        triggered = true;
+    }
+}
