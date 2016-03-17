dfc564bc70b3eae2e771871c25a2c77ce6d09d72
RyanFoxMDFung <ryanshiuhong.fung@mail.utoronto.ca>
2016-03-17 00:00:52 -0400

    integrating 2/3 3d obstacles
    -changed the obstaclespawner script a little bit
    -created colliders for both 3d obstacles
    -the spawn properly and interact properly.
    -need playtesting still

--- Unity/Skquirm/Assets/Scripts/ObstacleSpawn.cs
+++ Unity/Skquirm/Assets/Scripts/ObstacleSpawn.cs
@@ -4,7 +4,10 @@ using System.Collections;
 public class ObstacleSpawn : MonoBehaviour {
     public int maxNumOfObs;
     public float remainingTime; 
-    public GameObject obstacle;
+    public GameObject obstacle1;
+    public GameObject obstacle2;
+    public GameObject obstacle3;
+    private int pickint;
     private float xVar;
     private float zVar;
     private float spawnTime;
@@ -22,11 +25,25 @@ public class ObstacleSpawn : MonoBehaviour {
             spawnTime -= Time.deltaTime;
             if (spawnTime <= 0)
             {
+                pickint = Random.Range(0,3);
                 xVar = Random.Range(-37.0F, 25.0F);
                 zVar = Random.Range(-28.0F, 25.0F);
                 currentNumOfObs += 1;
-                Instantiate(obstacle, new Vector3(xVar , 100F, zVar), Quaternion.identity);
-                spawnTime = remainingTime;
+                if (pickint == 0)
+                {
+                    Instantiate(obstacle1, new Vector3(xVar, 100F, zVar), Quaternion.identity);
+                    spawnTime = remainingTime;
+                }
+                else if (pickint == 1)
+                {
+                    Instantiate(obstacle2, new Vector3(xVar, 100F, zVar), Quaternion.identity);
+                    spawnTime = remainingTime;
+                }
+                else
+                {
+                    Instantiate(obstacle3, new Vector3(xVar, 100F, zVar), Quaternion.identity);
+                    spawnTime = remainingTime;
+                }
             }
         }
 	}
