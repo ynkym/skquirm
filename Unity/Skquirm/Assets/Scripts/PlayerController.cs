6ee7b381b582a56e99c57a1437821b6215948c58
Alan Ricardo Gomes <Alan Ricardo Gomes>
2016-03-19 00:32:04 -0500

    water of the fisrt stage changed

--- Unity/Skquirm/Assets/Scripts/PlayerController.cs
+++ Unity/Skquirm/Assets/Scripts/PlayerController.cs
@@ -210,46 +210,49 @@ public class PlayerController : MonoBehaviour {
 
 void OnTriggerEnter(Collider other)
     {
-        //it doesnt teleport with every collision now, however it doesnt always teleport for some odd reason.
-        if (other.gameObject.CompareTag("pipe1"))
+        if (Application.loadedLevelName == "SewerScene")
         {
-            //transfers to pipe6 at coordinates X:22, Y:14, Z:18
-            gameObject.transform.position = new Vector3(22, 14, 18);
-        }
-        else if (other.gameObject.CompareTag("pipe2"))
-        {
-            //transfer to pipe 5 at coordinates X:20.5, Y:14, Z:-15
-            gameObject.transform.position = new Vector3(-17, 14, -17);
-        }
-        else if (other.gameObject.CompareTag("pipe3"))
-        {
-            //transfer to pipe 7 at coordinates X:-16, Y:14, Z:18
-            gameObject.transform.position = new Vector3(-16, 14, 18);
-        }
-        else if (other.gameObject.CompareTag("pipe4"))
-        {
-            //transfer to pipe 8 at coordinates X:20.5, Y:14, Z:-15
-            gameObject.transform.position = new Vector3(20.5f, 14, -15);
-        }
-        else if (other.gameObject.CompareTag("pipe5"))
-        {
-            //transfer to pipe 2 at coordinates X:3, Y:4, Z:26
-            gameObject.transform.position = new Vector3(3, 4, 26);
-        }
-        else if (other.gameObject.CompareTag("pipe6"))
-        {
-            //transfer to pipe 1 at coordinates X:2.5, Y:4, Z:-25
-            gameObject.transform.position = new Vector3(2.5f, 4, -25);
-        }
-        else if (other.gameObject.CompareTag("pipe7"))
-        {
-            //transfer to pipe 3 at coordinates X:28, Y:4, Z:1.5
-            gameObject.transform.position = new Vector3(28, 4, 1.5f);
-        }
-        else
-        {
-            //transfer to pipe 4 at coordinates X:-23, Y:4, Z:2
-            gameObject.transform.position = new Vector3(-23, 4, 2);
+            //it doesnt teleport with every collision now, however it doesnt always teleport for some odd reason.
+            if (other.gameObject.CompareTag("pipe1"))
+            {
+                //transfers to pipe6 at coordinates X:22, Y:14, Z:18
+                gameObject.transform.position = new Vector3(22, 14, 18);
+            }
+            else if (other.gameObject.CompareTag("pipe2"))
+            {
+                //transfer to pipe 5 at coordinates X:20.5, Y:14, Z:-15
+                gameObject.transform.position = new Vector3(-17, 14, -17);
+            }
+            else if (other.gameObject.CompareTag("pipe3"))
+            {
+                //transfer to pipe 7 at coordinates X:-16, Y:14, Z:18
+                gameObject.transform.position = new Vector3(-16, 14, 18);
+            }
+            else if (other.gameObject.CompareTag("pipe4"))
+            {
+                //transfer to pipe 8 at coordinates X:20.5, Y:14, Z:-15
+                gameObject.transform.position = new Vector3(20.5f, 14, -15);
+            }
+            else if (other.gameObject.CompareTag("pipe5"))
+            {
+                //transfer to pipe 2 at coordinates X:3, Y:4, Z:26
+                gameObject.transform.position = new Vector3(3, 4, 26);
+            }
+            else if (other.gameObject.CompareTag("pipe6"))
+            {
+                //transfer to pipe 1 at coordinates X:2.5, Y:4, Z:-25
+                gameObject.transform.position = new Vector3(2.5f, 4, -25);
+            }
+            else if (other.gameObject.CompareTag("pipe7"))
+            {
+                //transfer to pipe 3 at coordinates X:28, Y:4, Z:1.5
+                gameObject.transform.position = new Vector3(28, 4, 1.5f);
+            }
+            else
+            {
+                //transfer to pipe 4 at coordinates X:-23, Y:4, Z:2
+                gameObject.transform.position = new Vector3(-23, 4, 2);
+            }
         }
     }
 
